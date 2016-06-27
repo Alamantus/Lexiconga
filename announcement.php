a3b549fc1821416371163ca0f3fb16ff5babc013
Robbie Antenesse <gamedev@alamantus.com>
2016-06-27 16:38:45 -0600

    Update the redirect to point to new repository name.

--- announcement.php
+++ announcement.php
@@ -1,1 +1,1 @@
-<i><small>[Release Date]</small></i> | So much has been updated! Check the <a href="http://test.lexicon.ga/updates" target="_blank">Updates page</a> to see all the changes!
+<i><small>[Release Date]</small></i> | So much has been updated! Check the <a href="/updates" target="_blank">Updates page</a> to see all the changes!
