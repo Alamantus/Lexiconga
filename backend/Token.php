e5ced20d273e8c5c3726fb23805513124d2b7b69
Robbie Antenesse <dev@alamantus.com>
2017-12-24 15:13:42 -0700

    Add Hashids to Token.php; catch bad token exceptions

--- backend/Token.php
+++ backend/Token.php
@@ -1,11 +1,21 @@
 <?php
 require_once('./vendor/firebase/php-jwt/src/JWT.php');
+require_once('./vendor/firebase/php-jwt/src/BeforeValidException.php');
+require_once('./vendor/firebase/php-jwt/src/ExpiredException.php');
+require_once('./vendor/firebase/php-jwt/src/SignatureInvalidException.php');
+require_once('./vendor/hashids/hashids/lib/Hashids/HashGenerator.php');
+require_once('./vendor/hashids/hashids/lib/Hashids/Hashids.php');
+
 use \Firebase\JWT\JWT;
+use \Hashids\Hashids;
+
 
 class Token {
   private $key;
+  private $hashids;
   function __construct() {
     $this->key = 'ˈkɑːn.læŋ.ɪŋ_4eva';
+    $this->hashids = new Hashids($this->key, 10);
   }
 
   public function encode ($data) {
@@ -13,6 +23,18 @@ class Token {
   }
 
   public function decode ($token) {
-    return JWT::decode($token, $this->key, array('HS256'));
+    try {
+      return JWT::decode($token, $this->key, array('HS256'));
+    } catch (Exception $ex) {
+      return false;
+    }
+  }
+
+  public function hash ($id) {
+    return $this->hashids->encode($id);
+  }
+
+  public function unhash ($hash) {
+    return $this->hashids->decode($hash);
   }
 }
