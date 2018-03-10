09a545eb5d930811f8ace1274c6d97c807f91d16
Robbie Antenesse <dev@alamantus.com>
2018-03-10 11:19:56 -0700

    Add bulma-checkradio for prettier checkboxes

--- src/components/management/AccountManager/MyAccount.jsx
+++ src/components/management/AccountManager/MyAccount.jsx
@@ -30,51 +30,53 @@ export class MyAccount extends Component {
   render() {
     return (
       <div>
-        <div className="columns">
+        <div className='columns'>
         
-          <div className="column">
-            <h2 className="title">Account Details</h2>
-            <div className="control">
+          <div className='column'>
+            <h2 className='title'>Account Details</h2>
+            <div className='control'>
               <strong>Email:</strong> <span>{this.state.email}</span>
             </div>
-            <div className="control">
+            <div className='control'>
               <strong>Username:</strong> <span>{this.state.username}</span>
             </div>
-            <div className="field">
-              <label className="label">
+            <div className='field'>
+              <label className='label'>
                 <span>Public Name:</span>
               </label>
-              <div className="control">
-                <input className="input" type="text" value={this.state.publicName}
+              <div className='control'>
+                <input className='input' type='text' value={this.state.publicName}
                   onInput={(event) => {this.setState({publicName: event.target.value})}} />
               </div>
             </div>
-            <div className="field">
-              <div className="control">
-                <label className="label checkbox">
-                  <input type="checkbox" checked={this.state.allowEmails ? 'checked' : null}
-                    onChange={(event) => { this.setState({ allowEmails: event.target.checked }) }} />
-                  <span>Allow Emails</span>
+            <div className='field'>
+              <div className='control'>
+                <input className='is-checkradio is-rtl' type='checkbox' id='allowEmails'
+                  checked={this.state.allowEmails ? 'checked' : false}
+                  onChange={(event) => { this.setState({ allowEmails: !this.state.allowEmails }) }} />
+                <label className='label is-unselectable' htmlFor='allowEmails'>
+                  Allow Emails
                 </label>
               </div>
             </div>
-            <div className="field">
-              <label className="label checkbox">
-                <input type="checkbox" checked={this.state.useIPAPronunciation ? 'checked' : null}
-                  onChange={(event) => { this.setState({ useIPAPronunciation: event.target.checked }) }} />
-                <span>Use IPA in Pronunciation Fields</span>
+            <div className='field'>
+              <input className='is-checkradio is-rtl' type='checkbox' id='useIPAPronunciation'
+                checked={this.state.useIPAPronunciation ? 'checked' : false}
+                onChange={(event) => { this.setState({ useIPAPronunciation: !this.state.useIPAPronunciation }) }} />
+              <label className='label is-unselectable' htmlFor='useIPAPronunciation'>
+                Use IPA in Pronunciation Fields
               </label>
             </div>
           </div>
           
-          <div className="column">
-            <h2 className="title">Account Actions</h2>
-            <div className="field">
-              <label className="label">
+          <div className='column'>
+            <h2 className='title'>Account Actions</h2>
+            <div className='field'>
+              <label className='label is-unselectable'>
                 <span>Change Dictionary</span>
               </label>
-              <div className="control">
-                <div className="select">
+              <div className='control'>
+                <div className='select'>
                   <select>
                     {this.props.userDictionaries.map(item => {
                       return <option value={item.id}>{item.name}</option>;
