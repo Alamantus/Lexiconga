401779adb1218d05ee1972d344047ab504239b45
Robbie Antenesse <dev@alamantus.com>
2019-05-23 12:04:29 -0600

    Fix variable typo in Dictionary->getDetails()

--- src/php/api/Dictionary.php
+++ src/php/api/Dictionary.php
@@ -118,7 +118,7 @@ VALUES ($new_id, ?, ?, ?, ?)";
           'isComplete' => $result['is_complete'] === '1' ? true : false,
           'isPublic' => $result['is_public'] === '1' ? true : false,
         ),
-        'lastUpdated' => is_null($result['last_updated']) ? $results['created_on'] : $result['last_updated'],
+        'lastUpdated' => is_null($result['last_updated']) ? $result['created_on'] : $result['last_updated'],
         'createdOn' => $result['created_on'],
       );
     }
