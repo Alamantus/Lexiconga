f0b146e5fcd070ec101ebb38a3ddcc13964dd593
Robbie Antenesse <dev@alamantus.com>
2019-05-23 15:26:13 -0600

    Remove messages from initialize; chenge when token login happens

--- src/index.js
+++ src/index.js
@@ -2,24 +2,22 @@ import './main.scss';
 
 import setupListeners from './js/setupListeners';
 import { renderAll } from './js/render';
-import { generateRandomWords, addMessage, hasToken } from './js/utilities';
+import { hasToken } from './js/utilities';
 import { loadDictionary } from './js/dictionaryManagement';
 import { loadSettings } from './js/settings';
 
 function initialize() {
-  addMessage('Loading...');
+  loadDictionary();
+  loadSettings();
+  setupListeners();
+
   if (hasToken()) {
     import('./js/account/index.js').then(account => {
       account.loginWithToken();
     });
   }
-  loadDictionary();
-  loadSettings();
-  // generateRandomWords(100);
-  setupListeners();
+  
   renderAll();
-  addMessage('Done Loading!');
-  // console.log('Rendered!');
 }
 
 window.onload = (function (oldLoad) {
