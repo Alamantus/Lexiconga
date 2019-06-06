9fe0da58dbb56cbd3ee94cfefb680f0705f05762
Robbie Antenesse <dev@alamantus.com>
2019-06-06 11:47:21 -0600

    Sort words in public view correctly

--- src/js/view/wordManagement.js
+++ src/js/view/wordManagement.js
@@ -1,4 +1,15 @@
 import { wordExists, getHomonymnIndexes } from "./utilities";
+import removeDiacritics from "../StackOverflow/removeDiacritics";
+
+export function sortWords() {
+  const { sortByDefinition } = window.currentDictionary.settings;
+  const sortBy = sortByDefinition ? 'definition' : 'name';
+
+  window.currentDictionary.words.sort((wordA, wordB) => {
+    if (removeDiacritics(wordA[sortBy]).toLowerCase() === removeDiacritics(wordB[sortBy]).toLowerCase()) return 0;
+    return removeDiacritics(wordA[sortBy]).toLowerCase() > removeDiacritics(wordB[sortBy]).toLowerCase() ? 1 : -1;
+  });
+}
 
 export function parseReferences(detailsMarkdown) {
   const references = detailsMarkdown.match(/\{\{.+?\}\}/g);
