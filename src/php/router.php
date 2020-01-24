a9ae7f938cf533a7834b0b3dcb7833fb4afc0eb3
Robbie Antenesse <dev@alamantus.com>
2020-01-24 15:57:31 -0700

    Update ads and bump patch version

--- src/php/router.php
+++ src/php/router.php
@@ -134,7 +134,7 @@ switch ($view) {
       oldLoad && oldLoad();
       if (UpUp) {
         UpUp.start({
-          'cache-version': '2.1.5',
+          'cache-version': '2.1.6',
           'content-url': 'offline.html',
           'assets': [
             \"" . implode('","', $files) . "\"
